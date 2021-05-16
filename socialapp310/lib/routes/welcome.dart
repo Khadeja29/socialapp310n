636c7783309387a308461de50cecf932feb4d593
s21aeshmawy <aeshmawy@sabanciuniv.edu>
2021-05-16 13:33:28 +0300

    modified:   android/app/google-services.json 	new file:   assets/images/googlelogo.png 	modified:   lib/routes/splashpage.dart 	modified:   lib/routes/welcome.dart 	modified:   pubspec.yaml

--- socialapp310/lib/routes/welcome.dart
+++ socialapp310/lib/routes/welcome.dart
@@ -1,7 +1,8 @@
 
-
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:socialapp310/utils/color.dart';
 import 'package:socialapp310/utils/styles.dart';
@@ -117,12 +118,129 @@ class _WelcomeState extends State<Welcome> {
                           ),
                         ),
                       ),
+                      SizedBox(height:20),
+                      GoogleSignInButton(),
                     ],
                   )
                 ],
               )
 
           )
+      ),
+    );
+  }
+}
+
+class Authentication {
+  static Future<User> signInWithGoogle({BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User user;
+
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount googleSignInAccount =
+    await googleSignIn.signIn();
+
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      try {
+        print("here1");
+        final UserCredential userCredential =
+        await auth.signInWithCredential(credential);
+        print("here2");
+        user = userCredential.user;
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'account-exists-with-different-credential') {
+          // handle the error here
+        }
+        else if (e.code == 'invalid-credential') {
+          // handle the error here
+        }
+      } catch (e) {
+        // handle the error here
+      }
+    }
+
+    return user;
+  }
+}
+
+class GoogleSignInButton extends StatefulWidget {
+  @override
+  _GoogleSignInButtonState createState() => _GoogleSignInButtonState();
+}
+
+class _GoogleSignInButtonState extends State<GoogleSignInButton> {
+  bool _isSigningIn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: _isSigningIn
+          ? CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.deepPurple),
+      )
+          : OutlinedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.white),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40),
+            ),
+          ),
+        ),
+        onPressed: () async {
+          setState(() {
+            _isSigningIn = true;
+          });
+
+          User user =
+          await Authentication.signInWithGoogle(context: context);
+
+          setState(() {
+            _isSigningIn = false;
+          });
+
+          if (user != null) {
+            Navigator.of(context).pushReplacementNamed('/homefeed');
+          }
+
+          setState(() {
+            _isSigningIn = false;
+          });
+        },
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                image: AssetImage("assets/images/googlelogo.png"),
+                height: 35.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  'Sign in with Google',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
