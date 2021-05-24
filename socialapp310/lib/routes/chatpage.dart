2535a81736443a0c7471b5449290be57e2b0ec62
saifulmalook2 <76789810+saifulmalook2@users.noreply.github.com>
2021-05-25 01:04:20 +0500

    so we need 1. 'Send' functionality  2. Not to show your user 3. Show msg under username and they're all for Chats, we only need UI for Chat

---
+++ socialapp310/lib/routes/chatpage.dart
@@ -0,0 +1,148 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:socialapp310/models/user.dart';
+
+class ChatPage extends StatefulWidget {
+  final docs;
+
+  const ChatPage({Key key, this.docs}) : super(key: key);
+
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  String groupChatId;
+  String userID;
+
+  TextEditingController textEditingController = TextEditingController();
+
+  ScrollController scrollController = ScrollController();
+  @override
+  void initState() {
+    getGroupChatId();
+    super.initState();
+  }
+
+  getGroupChatId() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+    userID = sharedPreferences.getString('Username');
+
+    String anotherUserId = widget.docs['Username'];
+
+    if (userID.compareTo(anotherUserId) > 0) {
+      groupChatId = '$userID - $anotherUserId';
+    } else {
+      groupChatId = '$anotherUserId - $userID';
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat page!'),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('messages')
+            .doc(groupChatId)
+            .collection(groupChatId)
+            .orderBy('timestamp', descending: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data != null) {
+            return Column(
+              children: <Widget>[
+                Expanded(
+                    child: ListView.builder(
+                      controller: scrollController,
+                      itemBuilder: (listContext, index) =>
+                          buildItem(snapshot.data.docs[index]),
+                      itemCount: snapshot.data.docs.length,
+                      reverse: true,
+                    )),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        controller: textEditingController,
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: () => sendMsg(),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          } else {
+            return Center(
+                child: SizedBox(
+                  height: 36,
+                  width: 36,
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                  ),
+                ));
+          }
+        },
+      ),
+    );
+  }
+
+  sendMsg() {
+    String msg = textEditingController.text.trim();
+
+    /// Upload images to firebase and returns a URL
+    if (msg?.isEmpty ?? false) {
+      print('thisiscalled $msg');
+      var ref = FirebaseFirestore.instance
+          .collection('messages')
+          .doc(groupChatId)
+          .collection(groupChatId)
+          .doc(DateTime.now().millisecondsSinceEpoch.toString());
+
+      FirebaseFirestore.instance.runTransaction((transaction) async {
+        await transaction.set(ref, {
+          "senderId": userID,
+          "anotherUserId": widget.docs['Username'],
+          "timestamp": DateTime.now().millisecondsSinceEpoch.toString(),
+          'content': msg,
+          "type": 'text',
+        });
+      });
+
+      scrollController.animateTo(0.0,
+          duration: Duration(milliseconds: 100), curve: Curves.bounceInOut);
+    } else {
+      print('Please enter some text to send');
+    }
+  }
+
+  buildItem(doc) {
+    return Padding(
+      padding: EdgeInsets.only(
+          top: 8.0,
+          left: ((doc['senderId'] == userID) ? 64 : 0),
+          right: ((doc['senderId'] == userID) ? 0 : 64)),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+            color: ((doc['senderId'] == userID)
+                ? Colors.grey
+                : Colors.greenAccent),
+            borderRadius: BorderRadius.circular(8.0)),
+        child: (doc['tyoe'] == 'text')
+            ? Text('${doc['content']}')
+            : Image.network(doc['content']),
+      ),
+    );
+  }
+}
--- socialapp310/lib/routes/homefeed/HomeFeed.dart
+++ socialapp310/lib/routes/homefeed/HomeFeed.dart
@@ -2,7 +2,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:socialapp310/chats_page.dart';
+import 'package:socialapp310/routes/chatspage.dart';
 import 'package:socialapp310/models/post.dart';
 import 'package:socialapp310/routes/homefeed/postCard.dart';
 import 'package:socialapp310/routes/welcome.dart';
